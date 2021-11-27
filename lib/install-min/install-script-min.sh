73fc1e4f3e378a4c5b58da75bd20ee54e16e668a
Jitzek <jitz_k@hotmail.com>
2021-11-28 00:35:34 +0100

    fixed some errors (part 3/?)

--- lib/install-min/install-script-min.sh
+++ lib/install-min/install-script-min.sh
@@ -80,7 +80,7 @@ function main() {
     $(
         if [[ ! -z $partition_scheme_root ]] && [[ -z $partition_scheme_home ]]; then
             echo "echo ${block_device}4 >> /etc/fstab"
-            echo "blkid '${block_device}4' | awk '{print $2}') | tee --append /etc/fstab"
+            echo "blkid '${block_device}4' | awk '{print \$2}') | tee --append /etc/fstab"
         fi
     )
 
