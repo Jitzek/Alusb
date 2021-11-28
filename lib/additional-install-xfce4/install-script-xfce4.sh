d3ab21c09f24b139aa314ff9c54cab7f14cf2194
Jitzek <jitz_k@hotmail.com>
2021-11-28 13:42:15 +0100

    Enable lightdm after install

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -5,6 +5,7 @@ function main() {
     sudo pacman --noconfirm -Syu
     sudo pacman --noconfirm -S xorg base-devel
     sudo pacman --noconfirm -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings git
+    sudo systemctl enable lightdm
     git clone https://aur.archlinux.org/yay-git.git
     $(cd ./yay-git && makepkg -si --noconfirm)
     yay --noconfirm -Syu pamac-aur
