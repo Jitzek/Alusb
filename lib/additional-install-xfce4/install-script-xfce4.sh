474c171dcfe5f9042c0368206941f8faf6862e09
Jitzek <jitz_k@hotmail.com>
2021-11-29 00:06:26 +0100

    Additional payloads

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -6,19 +6,22 @@ source "${_DIR_XFCE4}/form-xfce4.sh"
 
 ## Configurable variables ##
 home_dir=""
-additional_packages=("firefox" "file-roller" "gvfs")
+prerequisites=("xmlstarlet")
+base_packages=("xfce4" "xfce4-goodies" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "git")
+additional_pacman_packages=("firefox" "file-roller" "gvfs", "catfish")
 
 
 function main() {
+    sudo pacman --noconfirm -Syu "${prerequisites[@]}"
+
     ## Fill all user determined variables
     form_xfce4
 
     ################
     ###   Base   ###
     ################
-    sudo pacman --noconfirm -Syu
     sudo pacman --noconfirm -S xorg base-devel
-    sudo pacman --noconfirm -S xfce4 xfce4-goodies lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings git
+    sudo pacman --noconfirm -S "${base_packages[@]}"
     sudo systemctl enable lightdm
     git clone https://aur.archlinux.org/yay-git.git
     $(cd ./yay-git && makepkg -si --noconfirm)
@@ -27,8 +30,13 @@ function main() {
     #########################
     ###   Configuration   ###
     #########################
-    sudo pacman --noconfirm -S "${additional_packages[@]}"
+    sudo pacman --noconfirm -S "${additional_pacman_packages[@]}"
     tar -xvzf "${_DIR_XFCE4}/payloads.tar.gz" -C "${_DIR_XFCE4}"
+    cp -rf "${_DIR_XFCE4}/payloads/icons/*" "${home_dir}/.icons/"
+    cp -rf "${_DIR_XFCE4}/payloads/themes/*" "${home_dir}/.themes/"
+    cp -rf "${_DIR_XFCE4}/payloads/config/gtk-2.0" "${home_dir}/.config/"
+    cp -rf "${_DIR_XFCE4}/payloads/config/gtk-3.0" "${home_dir}/.config/"
+    cp -rf "${_DIR_XFCE4}/payloads/config/xfce-perchannel-xml" "${home_dir}/.config/xfce4/xfconf/"
 }
 
 main
