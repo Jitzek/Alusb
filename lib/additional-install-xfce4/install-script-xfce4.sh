7151e6bfb59df298198ebba03db449e81f3ceb74
Jitzek <jitz_k@hotmail.com>
2021-11-29 19:16:27 +0100

    More configurations

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -8,10 +8,10 @@ source "${_DIR_XFCE4}/form-xfce4.sh"
 ## Configurable variables ##
 home_dir=""
 prerequisites=("")
-base_packages=("xfce4" "xfce4-goodies" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet")
+base_packages=("xfce4" "xfce4-goodies" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol")
 # Maybe replace ttf-liberation with official microsoft fonts (see: https://wiki.archlinux.org/title/Microsoft_fonts#Installation)
 additional_yay_packages=("pamac-aur", "ttf-liberation") 
-additional_pacman_packages=("firefox" "file-roller" "gvfs" "catfish")
+additional_pacman_packages=("firefox" "firefox-adblock-plus" "file-roller" "gvfs" "catfish")
 
 function main() {
     mkdir ${_TEMP_XFCE4}
