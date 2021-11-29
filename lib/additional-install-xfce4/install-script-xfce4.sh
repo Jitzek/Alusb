c46312412d8aa0a3ba4f29493c05c7943e59876e
Jitzek <jitz_k@hotmail.com>
2021-11-29 04:01:11 +0100

    Removed yay after install

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -25,8 +25,8 @@ function main() {
     sudo pacman --noconfirm -S xorg base-devel
     sudo pacman --noconfirm -S "${base_packages[@]}"
     sudo systemctl enable lightdm
-    git clone https://aur.archlinux.org/yay-git.git
-    $(cd ./yay-git && makepkg -si --noconfirm)
+    git clone https://aur.archlinux.org/yay-git.git ${_TEMP_XFCE4}/yay
+    $(cd ${_TEMP_XFCE4}/yay && makepkg -si --noconfirm)
     yay --noconfirm -Syu "${additional_yay_packages[@]}"
     sudo pacman --noconfirm -S "${additional_pacman_packages[@]}"
 
@@ -38,9 +38,9 @@ function main() {
     mkdir ~/.config/xfce4 ~/.config/xfce4/xfconf
     cp -rf ${_DIR_XFCE4}/payloads/home/icons/* ${home_dir}/.icons/
     # Clone Papirus icons
-    git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git ${_TEMP_XFCE4}
-    cp -rf ${_TEMP_XFCE4}/Papirus ${home_dir}/.icons/
-    cp -rf ${_TEMP_XFCE4}/Papirus-Dark ${home_dir}/.icons/
+    git clone https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git ${_TEMP_XFCE4}/papirus/
+    cp -rf ${_TEMP_XFCE4}/papirus/Papirus ${home_dir}/.icons/
+    cp -rf ${_TEMP_XFCE4}/papirus/Papirus-Dark ${home_dir}/.icons/
     cp -rf ${_DIR_XFCE4}/payloads/home/themes/* ${home_dir}/.themes/
     cp -rf ${_DIR_XFCE4}/payloads/home/config/gtk-2.0 ${home_dir}/.config/
     cp -rf ${_DIR_XFCE4}/payloads/home/config/gtk-3.0 ${home_dir}/.config/
