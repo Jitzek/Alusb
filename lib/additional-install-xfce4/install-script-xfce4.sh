2d35d218efccf059b05f9473f20a2ba6f8386e10
Jitzek <jitz_k@hotmail.com>
2021-11-30 14:25:07 +0100

    Even more payloads

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -11,7 +11,7 @@ prerequisites=("")
 base_packages=("xfce4" "xfce4-goodies" "lightdm" "lightdm-gtk-greeter" "lightdm-gtk-greeter-settings" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol")
 # Maybe replace ttf-liberation with official microsoft fonts (see: https://wiki.archlinux.org/title/Microsoft_fonts#Installation)
 additional_yay_packages=("pamac-aur" "ttf-liberation") 
-additional_pacman_packages=("firefox" "firefox-adblock-plus" "file-roller" "gvfs" "catfish")
+additional_pacman_packages=("firefox" "firefox-adblock-plus" "file-roller" "gvfs" "catfish" "gedit" "xed")
 
 function main() {
     mkdir ${_TEMP_XFCE4}
@@ -43,16 +43,15 @@ function main() {
     cp -rf ${_TEMP_XFCE4}/papirus/Papirus ${home_dir}/.icons/
     cp -rf ${_TEMP_XFCE4}/papirus/Papirus-Dark ${home_dir}/.icons/
     cp -rf ${_DIR_XFCE4}/payloads/home/themes/* ${home_dir}/.themes/
-    cp -rf ${_DIR_XFCE4}/payloads/home/config/gtk-2.0 ${home_dir}/.config/
-    cp -rf ${_DIR_XFCE4}/payloads/home/config/gtk-3.0 ${home_dir}/.config/
-    cp -rf ${_DIR_XFCE4}/payloads/home/config/xfce-perchannel-xml ${home_dir}/.config/xfce4/xfconf/
+    cp -rf ${_DIR_XFCE4}/payloads/home/config/* ${home_dir}/.config/
     for d in ${home_dir}/.icons/*/; do
         gtk-update-icon-cache "$d"
     done
     for d in ${home_dir}/.themes/*/; do
         gtk-update-icon-cache "$d"
     done
-
+    sudo cp -rf ${_DIR_XFCE4}/payloads/usr/share/gtksourceview-4/styles/* /usr/share/gtksourceview-4/styles/
+    
     ####################
     ###   Clean-up   ###
     ####################
