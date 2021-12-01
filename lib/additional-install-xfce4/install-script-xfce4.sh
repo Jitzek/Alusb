a0fb4841462705bf0b91d3ce1f836a5ce6eea428
Jitzek <jitz_k@hotmail.com>
2021-12-01 18:01:15 +0100

    proper proper use of boolean variable

--- lib/additional-install-xfce4/install-script-xfce4.sh
+++ lib/additional-install-xfce4/install-script-xfce4.sh
@@ -63,7 +63,7 @@ function main() {
     ##################
     ###   Nvidia   ###
     ##################
-    if [[ $configure_nvidia ]]; then
+    if [ "$configure_nvidia" = true ]; then
         sudo pacman --noconfirm -S "${nvidia_packages[@]}"
         echo 'ACTION=="add", DEVPATH=="/bus/pci/drivers/nvidia", RUN+="/usr/bin/nvidia-modprobe -c0 -u"' | sudo tee /etc/udev/rules.d/70-nvidia.rules
         # sudo nvidia-xconfig
