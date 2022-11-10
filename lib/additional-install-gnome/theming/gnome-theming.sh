3b284955e5196435f598cfdf567ff10d32bf729f
Jitzek <developer@jitzek.com>
2022-11-10 20:54:47 +0100

    .

--- lib/additional-install-gnome/theming/gnome-theming.sh
+++ lib/additional-install-gnome/theming/gnome-theming.sh
@@ -11,8 +11,8 @@ function gnome_theming() {
 
     unzip "${_DIR_GNOME_THEMING}/payloads.zip" -d "${_TEMP_GNOME_THEMING}"
     
-    cp -rf ${_TEMP_GNOME_THEMING}/home/icons/* ${home_dir}/.icons/
-    cp -rf ${_TEMP_GNOME_THEMING}/home/themes/* ${home_dir}/.themes/
+    cp -rf ${_TEMP_GNOME_THEMING}/payloads/home/icons/* ${home_dir}/.icons/
+    cp -rf ${_TEMP_GNOME_THEMING}/payloads/home/themes/* ${home_dir}/.themes/
 
     cp -rf ${_TEMP_GNOME_THEMING}/payloads/home/config/* ${home_dir}/.config/
     cp -rf ${_TEMP_GNOME_THEMING}/payloads/home/.bashrc ${home_dir}
@@ -23,7 +23,7 @@ function gnome_theming() {
         gtk-update-icon-cache "$d"
     done
 
-    cp -rf ${_TEMP_GNOME_THEMING}/home/local/share/* ${home_dir}/.local/share/
+    cp -rf ${_TEMP_GNOME_THEMING}/payloads/home/local/share/* ${home_dir}/.local/share/
 
     sudo chmod +x ${_TEMP_GNOME_THEMING}/payloads/grub/install.sh
     $(cd ${_TEMP_GNOME_THEMING}/payloads/grub && sudo ./install.sh)
