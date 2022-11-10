abb22997b17ff55c766d368ec353a2ea96c279e8
Jitzek <developer@jitzek.com>
2022-11-10 20:44:24 +0100

    .

--- lib/additional-install-gnome/base/data/gnome-base-data.sh
+++ lib/additional-install-gnome/base/data/gnome-base-data.sh
@@ -4,8 +4,8 @@
 ###      Configurable variables      ###
 ########################################
 home_dir=""
-BASE_PACKAGES=("gnome" "gnome-tweaks" "gdm-settings" "thunar" "thunar-archive-plugin" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol" "file-roller" "appstream-glib" "wget")
-ADDITIONAL_YAY_PACKAGES=("pamac-flatpak-gnome" "ttf-liberation" "downgrade" "mugshot" "update-grub")
+BASE_PACKAGES=("gnome" "gnome-tweaks" "thunar" "thunar-archive-plugin" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol" "file-roller" "appstream-glib" "wget")
+ADDITIONAL_YAY_PACKAGES=("pamac-flatpak-gnome" "ttf-liberation" "downgrade" "mugshot" "update-grub" "gdm-settings")
 ADDITIONAL_PACMAN_PACKAGES=("firefox" "firefox-adblock-plus" "file-roller" "gvfs" "catfish" "gedit" "xed" "neofetch" "gparted" "firejail" "avahi" "cups" "cups-filters" "cups-pdf" "ghostscript" "gsfonts" "system-config-printer" "w3m" "conky" "conky-manager" "conky-all" "lm-sensors")
 ########################################
 ###   ENDOF Configurable variables   ###
