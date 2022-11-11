da4ac2f1ea70635541337bfbfd68fa782cd7d746
Jitzek <developer@jitzek.com>
2022-11-11 12:24:37 +0100

    .

--- lib/additional-install-gnome/base/gnome-base.sh
+++ lib/additional-install-gnome/base/gnome-base.sh
@@ -8,11 +8,11 @@ _DIR_GNOME_BASE_TEMP=${_DIR_GNOME_BASE}/tmp/base
 #######################
 function gnome_base() {
     mkdir -p ${_DIR_GNOME_BASE_TEMP}
-    
+
     sudo pacman --noconfirm -S "${BASE_PACKAGES[@]}"
     git clone https://aur.archlinux.org/yay-git.git ${_DIR_GNOME_BASE_TEMP}/yay/
     $(cd ${_DIR_GNOME_BASE_TEMP}/yay && makepkg -si --noconfirm)
-    yay --noconfirm -Syu "${ADDITIONAL_YAY_PACKAGES[@]}"
+    yes | yay -Syu "${ADDITIONAL_YAY_PACKAGES[@]}"
     sudo pacman --noconfirm -S "${ADDITIONAL_PACMAN_PACKAGES[@]}"
     sudo systemctl enable cups.service
     sudo systemctl enable avahi-daemon.service
