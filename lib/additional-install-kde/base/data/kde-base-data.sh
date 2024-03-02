33a4209a54939dddcd1ee5e0893b61c0c89b7b82
Jitzek <developer@jitzek.com>
2024-03-02 17:51:46 +0100

    packages change

--- lib/additional-install-kde/base/data/kde-base-data.sh
+++ lib/additional-install-kde/base/data/kde-base-data.sh
@@ -7,7 +7,7 @@ home_dir=""
 # TODO
 BASE_PACKAGES=("plasma-meta" "plasma-wayland-session" "sddm" "thunar" "thunar-archive-plugin" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol" "file-roller" "appstream-glib" "wget")
 ADDITIONAL_YAY_PACKAGES=("pamac-nosnap" "ttf-liberation" "downgrade" "mugshot" "update-grub")
-ADDITIONAL_PACMAN_PACKAGES=("xfce4-terminal" "firefox" "file-roller" "gvfs" "catfish" "xed" "neofetch" "gparted" "firejail" "avahi" "cups" "cups-filters" "cups-pdf" "ghostscript" "gsfonts" "system-config-printer" "w3m" "conky" "conky-manager" "lm_sensors" "kde-accessibility-meta" "kde-graphics-meta" "kde-multimedia-meta" "kde-utilities-meta" "kde-network-meta" "kde-office-meta" "kcron" "kio-admin" "kjournald" "ksystemlog" "partitionmanager")
+ADDITIONAL_PACMAN_PACKAGES=("kwrite" "firefox" "file-roller" "gvfs" "catfish" "xed" "neofetch" "gparted" "firejail" "avahi" "cups" "cups-filters" "cups-pdf" "ghostscript" "gsfonts" "system-config-printer" "w3m" "conky" "conky-manager" "lm_sensors" "kde-accessibility-meta" "kde-graphics-meta" "kde-multimedia-meta" "kde-network-meta" "kde-office-meta"  "kde-system-meta" "kde-utilities-meta")
 TO_REMOVE_PACMAN_PACKAGES=()
 ########################################
 ###   ENDOF Configurable variables   ###
