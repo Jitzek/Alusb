85784797c38706b9b575d426a70da35f7add06f4
Jitzek <developer@jitzek.com>
2024-03-08 23:36:17 +0100

    removed packages with qt5 as dependency

--- lib/additional-install-kde/base/data/kde-base-data.sh
+++ lib/additional-install-kde/base/data/kde-base-data.sh
@@ -5,9 +5,9 @@
 ########################################
 home_dir=""
 # TODO
-BASE_PACKAGES=("plasma-meta" "sddm" "thunar" "thunar-archive-plugin" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol" "file-roller" "appstream-glib" "wget")
+BASE_PACKAGES=("plasma-meta" "plasma-framework5" "sddm" "thunar" "thunar-archive-plugin" "git" "pipewire" "pipewire-alsa" "pipewire-pulse" "pipewire-jack" "network-manager-applet" "pavucontrol" "file-roller" "appstream-glib" "wget")
 ADDITIONAL_YAY_PACKAGES=("pamac-nosnap" "ttf-liberation" "downgrade" "mugshot" "update-grub")
-ADDITIONAL_PACMAN_PACKAGES=("kwrite" "firefox" "file-roller" "gvfs" "catfish" "xed" "neofetch" "gparted" "firejail" "avahi" "cups" "cups-filters" "cups-pdf" "ghostscript" "gsfonts" "system-config-printer" "w3m" "conky" "conky-manager" "lm_sensors" "kde-accessibility-meta" "kde-graphics-meta" "kde-multimedia-meta" "kde-network-meta" "kde-office-meta"  "kde-system-meta" "kde-utilities-meta")
+ADDITIONAL_PACMAN_PACKAGES=("kwrite" "firefox" "file-roller" "gvfs" "catfish" "xed" "neofetch" "gparted" "firejail" "avahi" "cups" "cups-filters" "cups-pdf" "ghostscript" "gsfonts" "system-config-printer" "w3m" "conky" "conky-manager" "lm_sensors" "kde-accessibility-meta" "audiocd-kio" "dragon" "elisa" "ffmpegthumbs" "kdenlive" "kdeconnect" "kdenetwork-filesharing" "kget" "krfb" "ktorrent" "kde-system-meta" "kde-utilities-meta")
 TO_REMOVE_PACMAN_PACKAGES=()
 ########################################
 ###   ENDOF Configurable variables   ###
